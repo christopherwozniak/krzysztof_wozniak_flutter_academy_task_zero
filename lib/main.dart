5859cb4f96ef428102e9da31319484875427fbff
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2022-03-06 16:05:22 +0100

    Delete bad code

--- lib/main.dart
+++ lib/main.dart
@@ -16,88 +16,19 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-      ),
-      home: Scaffold(
-        appBar: NewGradientAppBar(
-            title: const Text('Krzysztof Woźniak'),
-            gradient: const LinearGradient(
-                colors: [Colors.blueAccent, Colors.yellow])),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    height: 120,
-                    color: Colors.red,
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    height: 120,
-                    color: Colors.purple,
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    height: 120,
-                    color: Colors.green,
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(children: [
-                Row(
-                  children: [
-                    Expanded(
-                        flex: 1,
-                        child: Container(
-                          height: 120,
-                          color: Colors.purple,
-                        )),
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        height: 120,
-                        color: Colors.brown,
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                        flex: 1,
-                        child: Container(
-                          height: 120,
-                          color: Colors.orange,
-                        )),
-                    Expanded(
-                      flex: 1,
-                      child: Container(height: 120, color: Colors.green),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(height: 120, color: Colors.grey),
-                    )
-                  ],
-                ),
-              ]),
-            )
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
         ),
-      ),
-    );
+        home: Scaffold(
+          appBar: NewGradientAppBar(
+              title: const Text('Krzysztof Woźniak'),
+              gradient: const LinearGradient(
+                  colors: [Colors.blueAccent, Colors.yellow])),
+        ));
   }
 }
+       
             // body: GridView.count(
             //   crossAxisCount: 2,
             //   children: List.generate(100, (index) {
